e89e2ba1c5585953997fe6dc61b188ddcacdcbbc
Adel Ahmed <adel15952@gmail.com>
2026-04-16 19:09:30 +0200

    Add NOT_FOUND translation key and integrate I18nService in UserService for localized error messages

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,10 +4,14 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
+import { I18nContext, I18nService } from 'nestjs-i18n';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private readonly i18n: I18nService,
+  ) {}
 
   async findUsers(): Promise<User[]> {
     const users = await this.userModel.find();
@@ -17,7 +21,10 @@ export class UserService {
   async findUserById(id: string): Promise<User> {
     const user = await this.userModel.findById(id);
     if (!user) {
-      throw new NotFoundException(`Not found user ${id}`);
+      // throw new NotFoundException(`Not found user ${id}`);
+      throw new NotFoundException(
+        this.i18n.t('test.NOT_FOUND', { lang: I18nContext.current().lang }),
+      );
     }
     return user;
   }
